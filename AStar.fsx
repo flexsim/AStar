4582f4f81fdff6be1cbdf51386254afa54a6a490
anthonyj <anthony.johnson@flexsim.com>
2017-07-13 17:20:42 -0600

    Updated tree to 17.2

--- AStar.fsx
+++ AStar.fsx
@@ -3042,7 +3042,7 @@ return 0;
    </node>
   </node>
  </node>
- <node f="42" dt="2"><name>release</name><data>17.1</data></node>
+ <node f="42" dt="2"><name>release</name><data>17.2</data></node>
  <node f="42" dt="2"><name>revision</name><data>.0</data></node>
- <node f="42" dt="2"><name>flexsim release</name><data>17.1</data></node>
+ <node f="42" dt="2"><name>flexsim release</name><data>17.2</data></node>
 </node></flexsim-tree>
