4e4ad24f9705fe135293ed1c9aa11e5c20496917
matthew.gillespie <devnull@localhost>
2017-12-18 13:40:42 -0700

    Updating release node to 18.0

--- AStar.fsx
+++ AStar.fsx
@@ -2988,7 +2988,7 @@ return 0;
    </node>
   </node>
  </node>
- <node f="42" dt="2"><name>release</name><data>17.2</data></node>
+ <node f="42" dt="2"><name>release</name><data>18.0</data></node>
  <node f="42" dt="2"><name>revision</name><data>.0</data></node>
- <node f="42" dt="2"><name>flexsim release</name><data>17.2</data></node>
+ <node f="42" dt="2"><name>flexsim release</name><data>18.0</data></node>
 </node></flexsim-tree>
