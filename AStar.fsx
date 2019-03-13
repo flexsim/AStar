eaef6e486668517ef5d105fbb9c2cfdce54e0621
Jacob Gillespie <jacob.gillespie@flexsim.com>
2019-03-13 13:34:27 -0600

    Fix some exceptions caused by the previous commit. FLEXSIM-5075

--- AStar.fsx
+++ AStar.fsx
@@ -930,10 +930,6 @@ nodepoint(objectfocus(c), 0);</data></node>
        <node f="40"><name></name></node>
        <node f="42" dt="4"><name>AStarNavigator</name><data>
         <node f="40"><name></name></node>
-        <node f="42"><name>classes</name>
-         <node f="40"><name></name></node>
-         <node f="42" dt="3"><name>AStar::AStarNavigator</name><data><coupling>/installdata/add/1/data/astar/AStarNavigator</coupling></data></node>
-        </node>
         <node f="442" dt="2"><name>dropscript</name><data>treenode ontoObject = param(1);
 double x = param(2);
 double y = param(3);
@@ -950,73 +946,59 @@ set(spatialy(asn), y);
 set(spatialz(asn), z);
 return asn;
 </data></node>
+        <node f="42" dt="2"><name>helptopic</name><data>AStarTool</data></node>
         <node f="42" dt="2"><name>windowtitle</name><data>A* Navigator</data></node>
         <node f="42" dt="2"><name>picture</name><data>modules\AStar\bitmaps\astarnavigatorsmall.png</data></node>
        </data></node>
        <node f="42" dt="4"><name>Barrier</name><data>
         <node f="40"><name></name></node>
-        <node f="42"><name>classes</name>
-         <node f="40"><name></name></node>
-         <node f="42" dt="3"><name>AStar::Barrier</name><data><coupling>/installdata/add/1/data/astar/Barrier</coupling></data></node>
-        </node>
         <node f="442" dt="2"><name>OnClick</name><data>if (clickcode() == LEFT_RELEASE)
 	modeleditmode("AStar::Barrier");</data></node>
         <node f="42" dt="2"><name>droppath</name><data>MAIN:/project/library/astar/Barrier</data>
          <node f="2000040"><name></name></node></node>
+        <node f="42" dt="2"><name>helptopic</name><data>WorkingWithAStar</data></node>
         <node f="42" dt="2"><name>tooltip</name><data>An area where travelers cannot go 
 or where travel direction is constrained</data></node>
         <node f="42" dt="2"><name>picture</name><data>modules\AStar\bitmaps\solidbarrier.bmp</data></node>
        </data></node>
        <node f="42" dt="4"><name>Divider</name><data>
         <node f="40"><name></name></node>
-        <node f="42"><name>classes</name>
-         <node f="40"><name></name></node>
-         <node f="42" dt="3"><name>AStar::Divider</name><data><coupling>/installdata/add/1/data/astar/Divider</coupling></data></node>
-        </node>
         <node f="442" dt="2"><name>OnClick</name><data>if (clickcode() == LEFT_RELEASE)
 	modeleditmode("AStar::Divider");</data></node>
         <node f="42" dt="2"><name>droppath</name><data>MAIN:/project/library/astar/Divider</data>
          <node f="2000040"><name></name></node></node>
+        <node f="42" dt="2"><name>helptopic</name><data>WorkingWithAStar</data></node>
         <node f="42" dt="2"><name>tooltip</name><data>A line or set of lines that blocks travel
 like a wall</data></node>
         <node f="42" dt="2"><name>picture</name><data>modules\AStar\bitmaps\divider.bmp</data></node>
        </data></node>
        <node f="42" dt="4"><name>Preferred Path</name><data>
         <node f="40"><name></name></node>
-        <node f="42"><name>classes</name>
-         <node f="40"><name></name></node>
-         <node f="42" dt="3"><name>AStar::PreferredPath</name><data><coupling>/installdata/add/1/data/astar/PreferredPath</coupling></data></node>
-        </node>
         <node f="442" dt="2"><name>OnClick</name><data>if (clickcode() == LEFT_RELEASE)
 	modeleditmode("AStar::PreferredPath");</data></node>
         <node f="42" dt="2"><name>droppath</name><data>MAIN:/project/library/astar/PreferredPath</data>
          <node f="2000040"><name></name></node></node>
+        <node f="42" dt="2"><name>helptopic</name><data>WorkingWithAStar</data></node>
         <node f="42" dt="2"><name>tooltip</name><data>A path that acts as a "magnet" for travelers to travel on</data></node>
         <node f="42" dt="2"><name>picture</name><data>modules\AStar\bitmaps\preferredpath.bmp</data></node>
        </data></node>
        <node f="42" dt="4"><name>Bridge</name><data>
         <node f="40"><name></name></node>
-        <node f="42"><name>classes</name>
-         <node f="40"><name></name></node>
-         <node f="42" dt="3"><name>AStar::Bridge</name><data><coupling>/installdata/add/1/data/astar/Bridge</coupling></data></node>
-        </node>
         <node f="442" dt="2"><name>OnClick</name><data>if (clickcode() == LEFT_RELEASE)
 	modeleditmode("AStar::Bridge");</data></node>
         <node f="42" dt="2"><name>droppath</name><data>MAIN:/project/library/astar/Bridge</data>
          <node f="2000040"><name></name></node></node>
+        <node f="42" dt="2"><name>helptopic</name><data>WorkingWithAStar</data></node>
         <node f="42" dt="2"><name>tooltip</name><data>A path that directly connects one location to another.</data></node>
         <node f="42" dt="2"><name>picture</name><data>modules\AStar\bitmaps\bridge.bmp</data></node>
        </data></node>
        <node f="42" dt="4"><name>Mandatory Path</name><data>
         <node f="40"><name></name></node>
-        <node f="42"><name>classes</name>
-         <node f="40"><name></name></node>
-         <node f="42" dt="3"><name>AStar::MandatoryPath</name><data><coupling>/installdata/add/1/data/astar/MandatoryPath</coupling></data></node>
-        </node>
         <node f="442" dt="2"><name>OnClick</name><data>if (clickcode() == LEFT_RELEASE)
 	modeleditmode("AStar::MandatoryPath");</data></node>
         <node f="42" dt="2"><name>droppath</name><data>MAIN:/project/library/astar/MandatoryPath</data>
          <node f="2000040"><name></name></node></node>
+        <node f="42" dt="2"><name>helptopic</name><data>WorkingWithAStar</data></node>
         <node f="42" dt="2"><name>tooltip</name><data>A path that designated travelers must travel on</data></node>
         <node f="42" dt="2"><name>picture</name><data>modules\AStar\bitmaps\mandatorypath.bmp</data></node>
        </data></node>
