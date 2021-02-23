e88803fbddce4a0a539cce430475b3612cb104fe
Phil BoBo <phil.bobo@flexsim.com>
2021-02-23 11:42:07 -0700

    FLEXSIM-6655 Fixed the A* heat map on a core profile OpenGL context.

--- AStarDLL/Grid.cpp
+++ AStarDLL/Grid.cpp
@@ -1017,7 +1017,7 @@ void Grid::drawHeatMap(TreeNode * view)
 	if (!textureID)
 		glGenTextures(1, &textureID);
 	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexImage2D(GL_TEXTURE_2D, 0, 4, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
