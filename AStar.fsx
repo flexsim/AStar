bc046e010b735f0a2a80bf41ebd96f25bdc57e38
Matthew Gillespie <matthew.gillespie@flexsim.com>
2022-10-10 10:33:58 -0600

    Updated release to 23.0

--- AStar.fsx
+++ AStar.fsx
@@ -6594,7 +6594,7 @@ if (isclasstype(obj, "AStar::Barrier") &amp;&amp; !isclasstype(obj, "AStar::Divi
    </node>
   </node>
  </node>
- <node f="42" dt="2"><name>release</name><data>22.2</data></node>
+ <node f="42" dt="2"><name>release</name><data>23.0</data></node>
  <node f="42" dt="2"><name>revision</name><data>.0</data></node>
- <node f="42" dt="2"><name>flexsim release</name><data>22.2</data></node>
+ <node f="42" dt="2"><name>flexsim release</name><data>23.0</data></node>
 </node></flexsim-tree>
