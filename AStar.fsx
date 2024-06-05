0df336c5f1933909d1129d7af1715496317e0169
Phil BoBo <phil.bobo@flexsim.com>
2024-06-05 13:02:47 -0600

    Updating version strings to 24.2.

--- AStar.fsx
+++ AStar.fsx
@@ -6792,7 +6792,7 @@ return updated;</data></node>
    </node>
   </node>
  </node>
- <node f="42" dt="2"><name>release</name><data>24.1</data></node>
+ <node f="42" dt="2"><name>release</name><data>24.2</data></node>
  <node f="42" dt="2"><name>revision</name><data>.0</data></node>
- <node f="42" dt="2"><name>flexsim release</name><data>24.1</data></node>
+ <node f="42" dt="2"><name>flexsim release</name><data>24.2</data></node>
 </node></flexsim-tree>
